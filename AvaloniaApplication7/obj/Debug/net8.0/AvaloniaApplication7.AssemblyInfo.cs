542c6e85de08ad98c107ce000e410b2977ac1c60
irreaaa <irispbmink@gmail.com>
2025-02-17 19:56:53 +0300

    first

--- AvaloniaApplication7/obj/Debug/net8.0/AvaloniaApplication7.AssemblyInfo.cs
+++ AvaloniaApplication7/obj/Debug/net8.0/AvaloniaApplication7.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AvaloniaApplication7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e212e7e229e9259cfd22246081bca772ae6bee97")]
 [assembly: System.Reflection.AssemblyProductAttribute("AvaloniaApplication7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AvaloniaApplication7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
